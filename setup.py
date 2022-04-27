e3ff2b5d0ca85f9801dd3338f21f9d50b3b9290c
Miguel Ponce-de-Leon <miguel.ponce@bsc.es>
2022-04-27 16:38:13 +0200

    fixing entry points

--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ if os.path.isfile(requirementPath):
         install_requires = f.read().splitlines()
 
 setuptools.setup(
-    name="tools4physicell",
-    version="0.0.1",
+    name="pctk",
+    version="0.1.0",
     author="Miguel Ponce de Leon",
     author_email="miguel.ponce@bsc.es",
     description="A tool for handling MultiCellDS output from PhysiCell and PhysiBoSS simulations.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/PhysiBoSS/tools4physicell",
+    url="https://github.com/PhysiBoSS/pctk",
     packages=setuptools.find_packages(where="src"   ),
     package_dir={"": "src"},
     classifiers=[
@@ -30,8 +30,8 @@ setuptools.setup(
     python_requires='>=3.6',
     install_requires=install_requires,
     entry_points={
-        'console_scripts': ['plot-time-course=tools4physicell.cmds.plot_time_course:main',
-                            'write-pov-files=tools4physicell.cmds.write_pov_files:main',
+        'console_scripts': ['plot-time-course=pctk.cmds.plot_time_course:main',
+                            'write-pov-files=pctk.cmds.write_pov_files:main',
                             ]
         
     }
