819422cb1019ca29982e5de3b5f3990349d326e4
mponce <miguel.ponce@bsc.es>
2022-06-09 11:21:26 +0200

    fix on panada replace series

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ import os
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+
+
 setuptools.setup(
     name="pctk",
-    version="0.1.0",
+    version="0.0.2",
     author="Miguel Ponce de Leon",
     author_email="miguel.ponce@bsc.es",
     description="PhysiCell ToolKit: a tool for handling MultiCellDS output from PhysiCell and PhysiBoSS simulations.",
@@ -20,9 +22,9 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
-    install_requires=['matplotlib', 'numpy', 'pandas', 'pytz', 'scipy', 'seaborn'],
-    # install_requires=install_requires,
+    python_requires='>=3.7',
+    setup_requires=['numpy', 'scipy', 'matplotlib',  'pandas',  'seaborn'],
+    install_requires=['numpy', 'scipy', 'matplotlib',  'pandas',  'seaborn'],
     entry_points={
         'console_scripts': ['plot-time-course=pctk.cmds.plot_time_course:main',
                             'write-pov-files=pctk.cmds.write_pov_files:main',
