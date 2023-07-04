14a766d6e6abf08b3dbd2b6f68b58a8784d5c18d
Miguel Ponce-de-Leon <miguel.ponce@bsc.es>
2023-07-04 19:30:44 +0200

    keep track of this

--- src/pctk/cmds/summarize_simulation.py
+++ src/pctk/cmds/summarize_simulation.py
@@ -14,59 +14,7 @@ sys.path.append(modules_path)
 
 from multicellds import MultiCellDS
 
-def get_timeserie_mean(mcds, filter_alive=True):
-    time = []
-    values = []
-    filter_alive = True
-    for t, df in mcds.cells_as_frames_iterator():
-        time.append(t)
-        df = df.iloc[:,3:]
-        if filter_alive:
-            mask = df['current_phase'] <= 14
-            df = df[mask]
-        values.append(df.mean(axis=0).values)
 
-    cell_columns = df.columns.tolist()
-    df = pd.DataFrame(values, columns=cell_columns)
-    df['time'] = time
-    return df[['time'] + cell_columns]
-
-
-def get_timeserie_density(mcds):
-    data = []
-    for t,m in mcds.microenvironment_as_matrix_iterator():
-        data.append((t, m[5,:].sum()))
-    df = pd.DataFrame(data=data, columns=['time', 'tnf'])
-    return df
-
-def plot_molecular_model(df_cell_variables, list_of_variables, ax1):
-
-    threshold = 0.5
-
-    for label in list_of_variables:
-        y = df_cell_variables[label]
-        time = df_cell_variables["time"]
-        ax1.plot(time, y, label="% X " + label)
-
-    ax1.set_ylabel("% X")
-    ax1.yaxis.grid(True)
-    ax1.set_xlim((0,time.values[-1]))
-    ax1.set_ylim((0,1))
-    # ax1.set_xlabel("time (min)")
-    
-def plot_cells(df_time_course, color_dict, ax):
-
-    # Alive/Apoptotic/Necrotic vs Time
-    for k in color_dict:
-        ax.plot(df_time_course.time, df_time_course[k], "-", c=color_dict[k], label=k)
-    
-    # setting axes labels
-    # ax.set_xlabel("time (min)")
-    ax.set_ylabel("Nº of cells")
-    
-    # Showing legend
-    ax.legend()
-    ax.yaxis.grid(True)
 
 def main():
 
